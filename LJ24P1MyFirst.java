1ae285a7852851daf9cd7c961b8233146329d01b
Priyanshu279 <279priii@gmail.com>
2023-12-18 10:41:21 +0530

    java practice programs

---
+++ LJ24P1MyFirst.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class LJ24P1MyFirst
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Hello World");
+	}
+}
---
+++ LJ24P2CLArgument.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+class LJ24P2CLArgument
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Welcome");
+		System.out.println(args[0]);
+	}
+}
