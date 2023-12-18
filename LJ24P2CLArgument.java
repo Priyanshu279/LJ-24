de8a4273c9dda360ba3c091bb0a8a47ce12d251b
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-12-18 10:48:37 +0530

    Update LJ24P2CLArgument.java

--- LJ24P2CLArgument.java
+++ LJ24P2CLArgument.java
@@ -4,7 +4,7 @@ class LJ24P2CLArgument
 {
 	public static void main(String [] args)
 	{
-		System.out.println("Welcome");
-		System.out.println(args[0]);
+		System.out.print("Welcome ");
+		System.out.print(args[0]);
 	}
 }
