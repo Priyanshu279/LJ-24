483b3f20d6a079608ab0fa8b1ad95d2c69ab3b09
Priyanshu279 <279priii@gmail.com>
2023-12-18 15:49:07 +0530

    Java taking input Program

---
+++ LJ24P3KeybRead.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+
+class LJ24P3KeybRead
+{
+	public static void main(String [] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(a);
+	}
+}
