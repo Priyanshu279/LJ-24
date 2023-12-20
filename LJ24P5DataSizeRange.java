7af4f7f7097c6a57b28e619bfbb6c058a86dd41a
Priyanshu279 <279priii@gmail.com>
2023-12-20 11:24:27 +0530

    Size of datatype

---
+++ LJ24P5DataSizeRange.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+class LJ24P5DataSizeRange
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Int min:"+Integer.MIN_VALUE);
+		System.out.println("Int max:"+Integer.MAX_VALUE);
+		System.out.println("Int size:"+Integer.SIZE);
+		System.out.println("Int size in Bytes:"+Integer.BYTES);
+	}
+}
