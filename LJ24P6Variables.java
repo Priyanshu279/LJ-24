349a22e8361b9ebcb5acd8facb21f02702992059
Priyanshu279 <279priii@gmail.com>
2023-12-21 11:54:22 +0530

    Variables

---
+++ LJ24P6Variables.java
@@ -0,0 +1,29 @@
+import java.lang.*;
+
+class LJ24P6Variables
+{
+	public static void main(String [] args)
+	{
+		byte b; //declaration
+		b = 5; //initiallisation 
+		short s = 123;
+		int i = 12345;
+		long l = 124924323;
+		float f = 12.42f;
+		double d = 123.4444424d;
+		char c = 'A';
+		boolean bb = true;
+		
+		System.out.println(b);
+		b = 99;
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(bb);
+
+	}
+}
