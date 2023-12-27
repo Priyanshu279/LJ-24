cf4ed912e467f687c601d75a9537a89a6febea12
Priyanshu279 <279priii@gmail.com>
2023-12-27 17:43:47 +0530

    Operators

---
+++ LJ24P8DataTypeDetails.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+import java.awt.*;
+
+class LJ24P8DataTypeDetails
+{
+	public static void main(String [] args)
+	{
+		int x = -5;
+		int y = 5;
+		System.out.println(Integer.toBinaryString(x));
+		System.out.println(Integer.toBinaryString(y));
+			
+	}
+}
---
+++ LJ24P9Arithmetic.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.util.*;
+
+class LJ24P9Arithmetic
+{
+	public static void main(String [] args)
+	{
+		int a=14, b=5;
+		float c=(float)a/b;
+		int r=a%b;
+		
+		System.out.println(c);
+		System.out.println(r);
+
+		byte a1=10;
+		short b1=15;
+		int c1=a1+b1;
+
+		System.out.println(c1);
+
+		float x=12.5f;
+		long y=123l;
+		float z=x*y;
+
+		System.out.println(z);
+
+		char p=40;
+		int q=30;
+		int s=p-q;
+		
+		System.out.println(s);
+		
+		System.out.println((10+20)/2);
+
+	}
+}
